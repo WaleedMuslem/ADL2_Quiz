dbc8b2faa21f5a7a74b9dba818728ba77eb41bc7
Dalany28 <145031274+Dalany28@users.noreply.github.com>
2023-10-11 22:33:28 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const _options = document.querySelector('.quiz-options');
 const _checkBtn = document.getElementById('check-answer');
 const _playAgainBtn = document.getElementById('play-again');
 const _backHome = document.getElementById('back-home');
-
+const _comment = document.getElementById('comment');
 const _result = document.getElementById('result');
 const _correctScore = document.getElementById('correct-score');
 const _totalQuestion = document.getElementById('total-question');
