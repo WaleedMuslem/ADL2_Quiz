a708edd3077577b24430564bfa68b1ede0fc9a05
Omar Takassa <146325782+OmarTakassa@users.noreply.github.com>
2023-10-12 13:45:54 +0200

    Fixing the timer app.js

--- src/app.js
+++ src/app.js
@@ -9,133 +9,166 @@ const _result = document.getElementById('result');
 const _correctScore = document.getElementById('correct-score');
 const _totalQuestion = document.getElementById('total-question');
 const _timer = document.getElementById('timer');
+const _catePage = document.getElementById('category-page');
 
 let correctAnswer = "",
-  correctScore = askedCount = 0,
-  totalQuestion = 3;
+    correctScore = askedCount = 0,
+    totalQuestion = 5;
 let selectedCategory = null;
+let timerInterval;
 
 const categoryButtons = {
-      math: document.getElementById('math-category'),
-      history: document.getElementById('history-category'),
-      computer: document.getElementById('computer-category')
+    math: document.getElementById('category-math'),
+    computer: document.getElementById('category-computer'),
+    history: document.getElementById('category-history')
+    
 };
 
 function eventListeners() {
-  _checkBtn.addEventListener('click', checkAnswer);
-      _playAgainBtn.addEventListener('click', restartQuiz);
+    _checkBtn.addEventListener('click', checkAnswer);
+    _playAgainBtn.addEventListener('click', restartQuiz);
 
-  Object.keys(categoryButtons).forEach(category => {
-    categoryButtons[category].addEventListener('click', () => {
-      selectedCategory = category;
-      restartQuiz();
+    Object.keys(categoryButtons).forEach(category => {
+        categoryButtons[category].addEventListener('click', () => {
+            selectedCategory = category;
+            restartQuiz(); // Restart the quiz when a category is selected
+        });
     });
-  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-      eventListeners();
-   _totalQuestion.textContent = totalQuestion;
-   _correctScore.textContent = correctScore;
-  loadQuestion();
+    eventListeners();
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+
+    const categoryImages = document.querySelectorAll('img[id^="category-"]');
+    categoryImages.forEach((image) => {
+        image.addEventListener('click', function () {
+            const category = image.id.replace('category-', ''); // Extract category name from the image id
+            selectedCategory = category;
+            loadQuestion();
+            
+            //hide img here after we click choosing cat
+            categoryImages.forEach((img) => {
+                img.style.display = 'none';
+            });
+
+            _catePage.style.display = "none";
+            _container.style.display = "block";
+        });
+    });
+
+
 });
 
-
+// by category
 async function loadQuestion() {
-  const category = selectedCategory || 'general'; 
-  const APIUrl = `https://opentdb.com/api.php?amount=1&category=${getCategoryId(category)}`;
-  const result = await fetch(`${APIUrl}`);
-  const data = await result.json();
-  _result.innerHTML = "";
-  showQuestion(data.results[0]);
+    clearInterval(timerInterval);
+    const category = selectedCategory || 'general'; 
+    const APIUrl = `https://opentdb.com/api.php?amount=1&category=${getCategoryId(category)}`;
+    const result = await fetch(`${APIUrl}`);
+    const data = await result.json();
+    _result.innerHTML = "";
+
+    showQuestion(data.results[0]);
+
 }
 
+// by category id HELPER
 function getCategoryId(category) {
-  switch (category) {
-      case 'math':
-      return 19;
-      case 'history':
-      return 23;
-      case 'computer':
-      return 18;
-      default:
-      return 9; 
-  }
+    switch (category) {
+        case 'math':
+            return 19;
+        case 'history':
+            return 23;
+        case 'computer':
+            return 18;
+        default:
+            return 9;
+    }
 }
 
 function startTimer() {
-  let timeLeft = 15;
-   _timer.textContent = `00:${timeLeft}`;
-  timerInterval = setInterval(() => {
-  timeLeft--;
-  _timer.textContent = `00:${timeLeft}`;
-
-  if (timeLeft <= 0) {
     clearInterval(timerInterval);
-    checkCount();
-  }
-  }, 1000);
+    let timeLeft = 15;
+    _timer.textContent = `00:${timeLeft}`;
+
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        _timer.textContent = `00:${timeLeft}`;
+
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+
+            checkCount();
+        }
+    }, 1000);
 }
 
 
 function showQuestion(data) {
-   _checkBtn.disabled = false;
-  correctAnswer = data.correct_answer;
-  let incorrectAnswer = data.incorrect_answers;
-  let optionsList = incorrectAnswer;
-  optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
-  _question.innerHTML = `${data.question} ` ;
-   _options.innerHTML = `
-   ${optionsList.map((option, index) => `
-   <li> ${index + 1}. <span>${option}</span> </li>
-   `).join('')}
-  `;
-  selectOption();
-  startTimer();
+    _checkBtn.disabled = false;
+    correctAnswer = data.correct_answer;
+    let incorrectAnswer = data.incorrect_answers;
+    let optionsList = incorrectAnswer;
+    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+
+    _question.innerHTML = `${data.question} ` ;
+    
+    _options.innerHTML = `
+        ${optionsList.map((option, index) => `
+            <li> <span>${option}</span> </li>
+        `).join('')}
+    `;
+    selectOption();
+    startTimer();
 }
+
+//options
 function selectOption() {
-  _options.querySelectorAll('li').forEach(function (option) {
-    option.addEventListener('click', function () {
-      if (_options.querySelector('.selected')) {
-       const activeOption = _options.querySelector('.selected');
-        activeOption.classList.remove('selected');
-      }
-      option.classList.add('selected');
+    _options.querySelectorAll('li').forEach(function (option) {
+        option.addEventListener('click', function () {
+            if (_options.querySelector('.selected')) {
+                const activeOption = _options.querySelector('.selected');
+                activeOption.classList.remove('selected');
+            }
+            option.classList.add('selected');
+        });
     });
-
-  });
 }
 
+//checking ans
 function checkAnswer() {
-  clearInterval(timerInterval);
-  _checkBtn.disabled = true;
-  if (_options.querySelector('.selected')) {
-    let selectedAnswer = _options.querySelector('.selected span').textContent;
-    if (selectedAnswer === HTMLDecode(correctAnswer)) {
-      correctScore++;
-      _result.innerHTML = "";
-    } else 
-    {
+    clearInterval(timerInterval);
+    _checkBtn.disabled = true;
+    if (_options.querySelector('.selected')) {
+        let selectedAnswer = _options.querySelector('.selected span').textContent;
+        if (selectedAnswer === HTMLDecode(correctAnswer)) {
+            correctScore++;
             _result.innerHTML = "";
-    }
-    checkCount();
-  }
-  else {
+        } else {
+            _result.innerHTML = "";
+        }
+        checkCount();
+    } else {
         _result.innerHTML = `<p>Please select an option!</p>`;
         _checkBtn.disabled = false;
-  }
+    }
+
 }
 
-function HTMLDecode(textString){
-  let doc = new DOMParser().parseFromString(textString, "text/html");
-  return doc.documentElement.textContent;
+
+function HTMLDecode(textString) {
+    let doc = new DOMParser().parseFromString(textString, "text/html");
+    return doc.documentElement.textContent;
 }
 
 function checkCount() {
-  askedCount++;
-  setCount();
-  _comment.innerHTML = '';
-  if (askedCount === totalQuestion) {
+    askedCount++;
+    setCount();
+    _comment.innerHTML = '';
+    _timer.innerHTML = '';
+    if (askedCount === totalQuestion) {
         setTimeout(function () {
             console.log("");
         }, 5000);
@@ -148,8 +181,7 @@ function checkCount() {
         _checkBtn.style.display = "none";
         _question.style.display = "none";
         _options.style.display = "none";
-    }
-  else {
+    } else {
         setTimeout(function () {
             loadQuestion();
         }, 300);
@@ -157,15 +189,21 @@ function checkCount() {
 }
 
 function setCount() {
-  correctScore = askedCount = 0;
-  _playAgainBtn.style.display = "none";
-  _backHome.style.display = "none";
-  _comment.style.display = "none";
-  _checkBtn.style.display = "block";
-  _question.style.display = "block";
-  _options.style.display = "block";
-  _checkBtn.disabled = false;
-  setCount();
-  loadQuestion();
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+}
+
+function restartQuiz() {
+    correctScore = askedCount = 0;
+    _playAgainBtn.style.display = "none";
+    _backHome.style.display = "none";
+    _comment.style.display = "none";
+    _checkBtn.style.display = "block";
+    _question.style.display = "block"; 
+    _options.style.display = "block";
+    _checkBtn.disabled = false;
+    _timer.style.display = "block";
+    setCount();
+    loadQuestion();
 }
   
